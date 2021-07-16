c93fec219ee14ac7c2fea08b9fe9b2ac49a80226
kjh090622 <86462104+kjh090622@users.noreply.github.com>
2021-07-16 15:36:36 +0900

    20210716 계산기

---
+++ 계산기.py
@@ -0,0 +1,23 @@
+"""
+더하기+
+빼기-
+곱하기*
+나누기/
+몫만 구하기//
+나머지만 구하기%
+"""
+num1 = 15
+num2 = 2
+result = num1 + num2
+print("더하기:%d" %(num1 + num2))
+result = num1 - num2
+print("빼기:%d" %(num1 - num2))
+result = num1 * num2
+print("곱하기:%d" %(num1 * num2))
+result = num1 / num2
+print("나누기:%.1f" %(num1 / num2))
+result = num1 // num2
+print("몫만 구하기:%d" %(num1 // num2))
+result = num1 % num2
+print("나머지만 구하기:%d" %(num1 % num2))
+
