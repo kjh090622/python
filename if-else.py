735ffc8821872a36476a4d27e8f9268d986f8942
kjh090622 <86462104+kjh090622@users.noreply.github.com>
2021-07-30 15:45:19 +0900

    20210730

---
+++ if-else.py
@@ -0,0 +1,14 @@
+"""
+if-else
+
+if(조건):
+    실행문
+else :
+    else 는 조건이 없다
+if에 해당하지 않는 모든것이 else
+"""
+score = int(input("점수를 입력해주세요"))
+if (score >= 80):
+    print("합격")
+else:
+    print("불합격")
