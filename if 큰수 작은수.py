67c7d8c46d7fd18c93aac1596888e881a20fd53d
kjh090622 <86462104+kjh090622@users.noreply.github.com>
2021-07-30 15:29:14 +0900

    20210730

---
+++ if 큰수 작은수.py
@@ -0,0 +1,8 @@
+num1 = int(input("수를 입력하시오"))
+num2 = int(input("수를 입력하시오"))
+if (num1 > num2):
+    print("큰수 = %d"%num1)
+if (num1 < num2):
+    print("큰수 = %d"%num2)
+if(num1 == num2):
+    print("두수가 같습니다")
