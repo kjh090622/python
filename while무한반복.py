540340250f5f25d6262d3e4cac509acd5f1bd522
kjh090622 <86462104+kjh090622@users.noreply.github.com>
2021-09-17 15:13:56 +0900

    20210917

---
+++ while무한반복.py
@@ -0,0 +1,13 @@
+"""
+while무한반복.py
+while의 조건이 계속 참이면 무한반복
+조건:True/False
+True : 계속 무한 반복
+False : 계속 거짓 > 한번도 실행되지 않는다
+"""
+while False :
+    print("while무한거짓")
+while  True:
+    print("while무한반복")
+    break
+  
